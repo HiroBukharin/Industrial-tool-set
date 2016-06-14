53c5e58e06434d11f264614152cb3f9d96e6072a
Hiro Bukharin <hiro.bukharin@gmail.com>
2016-06-14 03:30:20 +0000

    database script

---
+++ eve_blueprint_database_maker.py
@@ -0,0 +1,17 @@
+#!/usr/lib/python3.4
+
+#http://www.blog.pythonlibrary.org/2012/07/18/python-a-simple-step-by-step-sqlite-tutorial/
+
+import sqlite3
+
+
+conn = sqlite3.connect(DARPA_blueprints.db)
+
+c = conn.cursor()
+
+c.execute("""CREATE TABLE IF NOT EXISTS 
+        All_blueprints (ID INT PRIMAY KEY, time_efficiency INT, type_id INT,
+          type_name TEXT, quantity INT, location_id INT, material_efficiency INT,
+          run INT)"""
+
+          
